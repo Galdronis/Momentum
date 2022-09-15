e0d75bfee08b1ac52fbc63f03fc5c7dad803ba97
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-09-15 11:17:32 -0400

    Required the card route in index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const cardRoutes = require('./cardRoutes');
 
 router.use('/users', userRoutes)
-
+router.use('/cards', cardRoutes)
 module.exports = router;

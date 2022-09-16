72a242227084d77e26f8e2feb1b3d0ee9ba54d7b
Nicholas Bales <nickb96@icloud.com>
2022-09-16 11:20:01 -0400

    fixed login functionality

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -20,45 +20,44 @@ router.post('/', async (req, res) => {
     }
   });
 
-  router.post('/login', async (req, res) => {
-    console.log('hello')
-    try {
-      // Find the user who matches the posted e-mail address
-      const userData = await User.findOne({ where: { email: req.body.email } });
-      console.log(userData, "-------------");
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-      // Verify the posted password with the password store in the database
-      const validPassword = await userData.checkPassword(req.body.password);
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-      // Create session variables based on the logged in user
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-        res.json({ user: userData, message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      res.status(400).json(err);
+router.post('/login', async (req, res) => {
+  console.log('hello')
+  try {
+    // Find the user who matches the posted e-mail address
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    console.log(userData, "-------------");
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
     }
-  });
-
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
+    // Verify the posted password with the password store in the database
+    const validPassword = await userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    // Create session variables based on the logged in user
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json({ user: userData, message: 'You are now logged in!' });
     });
-  } else {
-    res.status(404).end();
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
+
+// router.get('/logout', async (request, response) => {
+//   if (request.session.user_id) {
+//       delete request.session.user_id;
+//       response.json({result: 'SUCCESS'});
+//   } else {
+//       response.json({result: 'ERROR', message: 'User is not logged in.'});
+//   }
+// });
 
 module.exports = router

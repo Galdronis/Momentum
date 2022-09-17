056b261bda363b9c2e824126a3fb497e6008f055
Nicholas Bales <nickb96@icloud.com>
2022-09-16 23:05:22 -0400

    edited code to allow for proper routing

--- public/js/userCreate.js
+++ public/js/userCreate.js
@@ -3,11 +3,15 @@ const createFormHandler = async (event) => {
     event.preventDefault();
     // Gather the data from the form elements on the page
     const email = document.querySelector('#enter-email').value.trim();
-    const password = document.querySelector('#enter-login').value.trim();
+    const password = document.querySelector('#enter-password').value.trim();
   
+    const package = {
+      email, password
+    }
     if (email && password) {
+      console.log(package)
       // Send the e-mail and password to the server
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/users/create', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
@@ -21,4 +25,4 @@ const createFormHandler = async (event) => {
     }
   };
   
-  document.querySelector('.create-form').addEventListener('submit', createFormHandler);
+    document.addEventListener('submit', createFormHandler);

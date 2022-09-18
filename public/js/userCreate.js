0d6e36e6d46dccbe937d6070e0b0bc8bb6a0bfcb
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-09-17 22:14:27 -0400

    Added name property to the user

--- public/js/userCreate.js
+++ public/js/userCreate.js
@@ -2,18 +2,19 @@ const createFormHandler = async (event) => {
     // Stop the browser from submitting the form so we can do so with JavaScript
     event.preventDefault();
     // Gather the data from the form elements on the page
+    const name = document.querySelector('#enter-name').value.trim();
     const email = document.querySelector('#enter-email').value.trim();
     const password = document.querySelector('#enter-password').value.trim();
   
     const package = {
-      email, password
+      name, email, password
     }
     if (email && password) {
       console.log(package)
       // Send the e-mail and password to the server
       const response = await fetch('/api/users/create', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
